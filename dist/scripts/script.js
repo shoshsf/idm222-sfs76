859d443dd9b830628a090168cbd6c3ac016c2cd1
shoshsf <72558473+shoshsf@users.noreply.github.com>
2023-01-02 19:42:57 -0500

    The Start

---
+++ dist/scripts/script.js
@@ -0,0 +1,18 @@
+// function openRightMenu() {
+//     document.getElementById("rightMenu").style.display = "block";
+// }
+
+// function closeRightMenu() {
+//     document.getElementById("rightMenu").style.display = "none";
+// }
+
+
+
+function w3_open() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
